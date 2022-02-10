a9c5a22dfe2887d7b40c2c21fe0ff8b373f15b6a
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-02-10 12:59:21 -0800

    worked on form for auth page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { getQueriesForElement } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+// import { getQueriesForElement } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
 import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch, NavLink, Redirect } from 'react-router-dom';
 import { getUser } from './services/fetch.utils';
--- src/AuthPage.js
+++ src/AuthPage.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { useState } from 'react';
 
 export default function AuthPage() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+    
   return (
-    <div>AuthPage</div>
-  )
+    <div className='auth'>
+      <h2>The Very Nice Restaurant Inventory</h2>
+      <form>
+        <label>Email
+          <input required type="email" value={email} onChange={e => setEmail(e.target.value)}/>
+        </label>  
+        <label>Password
+          <input required type="password" value={password} onChange={e => setPassword(e.target.value)}/>
+        </label>
+        <button>Sign Up</button>      
+      </form>
+      <button type="button">Sign In</button>
+    </div>
+  );
 }
 
 
