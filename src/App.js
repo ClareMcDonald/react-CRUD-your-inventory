c201f30812f70d26d1bf7ca3e0d0a3c0cd976c04
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-02-10 12:44:55 -0800

    started on App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import { getQueriesForElement } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch, NavLink, Redirect } from 'react-router-dom';
 import './App.css';
+import AuthPage from './AuthPage';
 
 function App() {
+  const [user, setUser] = useState(localStorage.getItem('supabase.auth.token'));
+
+  useEffect(() => {
+    async function fetch() {
+      const user = await getUser();
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+      
+        </header>
+        <main>
+          <Switch>
+            <Route exact path="/">
+              {
+                user
+                  ? <Redirect to='/restaurants' />
+                  : <AuthPage />
+              }
+            </Route>
+          </Switch>
+        </main>
+      </div>
+    </Router>
   );
 }
 
