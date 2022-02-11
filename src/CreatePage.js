b70c73b518955c13d5cdd4edae2377c2c724960e
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-02-11 12:16:02 -0800

    added onChange to form inputs

--- src/CreatePage.js
+++ src/CreatePage.js
@@ -7,20 +7,24 @@ export default function CreatePage() {
   const [formCity, setFormCity] = useState('');
   const [formPriceRating, setFormPriceRating] = useState('');
 
+  async function handleSubmit(e) {
+
+  }
+
   return (
     <div>Create a Restaurant
       <form>
         <label>Name
-          <input></input>
+          <input name="name" value={formName} onChange={e => setFormName(e.target.value)}/>
         </label>
         <label>Cuisine
-          <input></input>
+          <input name="cuisine" value={formCuisine} onChange={e => setFormCuisine(e.target.value)} />
         </label>
         <label>City
-          <input></input>
+          <input name="city" value={formCity} onChange={e => setFormCity(e.target.value)} />
         </label>
         <label>Price Rating
-          <input></input>
+          <input name="price-rating" value={formPriceRating} onChange={e => setFormPriceRating(e.target.value)} />
         </label>
         <button>Add Restaurant</button>
       </form>
