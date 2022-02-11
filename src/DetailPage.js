973f67d49cb32e46c7c117d004186e34c35215ec
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-02-11 14:15:04 -0800

    wrote useEffect for DetailPage

--- src/DetailPage.js
+++ src/DetailPage.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { getRestaurant } from './services/fetch.utils';
 
 export default function DetailPage() {
-  const id = useParams;
+  const params = useParams;
   const [singleRestaurant, setSingleRestaurant] = useState({});
     
+  useEffect(() => {
+    async function fetchRestaurant() {
+      const restaurant = await getRestaurant(params.id);
+        
+      setSingleRestaurant(restaurant);
+    }
+      
+    fetchRestaurant();
+  }, [params.id]);
+    
+
   return (
-    <div>DetailPage</div>
+    <h3>{singleRestaurant.name}</h3>
   );
 }
