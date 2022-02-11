55d70fe4197094e32f46c53940ccef49a6b67bba
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-02-11 12:57:13 -0800

    set state in DetailPage

--- src/DetailPage.js
+++ src/DetailPage.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 export default function DetailPage() {
+  const id = useParams;
+  const [singleRestaurant, setSingleRestaurant] = useState({});
+    
   return (
     <div>DetailPage</div>
   );
